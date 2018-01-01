fc081a6497182a81eddc27d0ace704422cd13444
cian2009 <g00337022@gmit.ie>
2018-01-01 21:40:02 +0000

    Added append set up for Fitness Records

--- FitnessServer/src/ie/fitnessapp/options/FitnessRecords.java
+++ FitnessServer/src/ie/fitnessapp/options/FitnessRecords.java
@@ -1,13 +1,23 @@
 package ie.fitnessapp.options;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class FitnessRecords {
 
-	public static void Records(int clientID, Socket clientSocket, ObjectOutputStream out, String userDetails) {
+	public static void Records(int clientID, Socket clientSocket, ObjectOutputStream out, String userDetails) throws IOException {
 		
-		System.out.println("Test");
+		PrintWriter writer = new PrintWriter(new FileOutputStream(new File("Users/"+userDetails+"/Fitness-Records.txt"), true /* append = true */));
+			
+		writer.println("Test \n");
+		
+		writer.close();
+		
+		
 		
 	}
 
