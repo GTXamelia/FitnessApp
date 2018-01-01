49d5c21bfdc4f91a1b94b3a2f15dce7382a4d8c3
cian2009 <g00337022@gmit.ie>
2018-01-01 14:17:02 +0000

    Register foundation operational

--- FitnessServer/src/ie/fitnessapp/server/UserOptionManager.java
+++ FitnessServer/src/ie/fitnessapp/server/UserOptionManager.java
@@ -1,5 +1,6 @@
 package ie.fitnessapp.server;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
@@ -15,20 +16,36 @@ public class UserOptionManager {
 	public static void Register(int clientID, Socket clientSocket) {
 		System.out.println("Client "+clientID+": Address - "+clientSocket.getInetAddress().getHostName()+" - connection accepted");
 		
+		File file = new File("Users/"+ie.fitnessapp.options.Register.getPPSN()+"/"+ie.fitnessapp.options.Register.getPPSN()+".txt");
 		
+		if(file.exists()){
+			System.out.println("Client "+clientID+": Address - "+clientSocket.getInetAddress().getHostName()+" - profile already exsists");
+		}else{
+			System.out.println("Client "+clientID+": Address - "+clientSocket.getInetAddress().getHostName()+" - register successful");
+			file.getParentFile().mkdirs();
+			
 			try {
-				PrintWriter writer = new PrintWriter("/Users/"+ie.fitnessapp.options.Register.getPPSN()+"/"+ie.fitnessapp.options.Register.getPPSN()+".txt", "UTF-8");
+				PrintWriter writer = new PrintWriter("Users/"+ie.fitnessapp.options.Register.getPPSN()+"/"+ie.fitnessapp.options.Register.getPPSN()+".txt", "UTF-8");
 				
+				writer.println(ie.fitnessapp.options.Register.getName());
+				writer.println(ie.fitnessapp.options.Register.getAddess());
 				writer.println(ie.fitnessapp.options.Register.getPPSN());
+				writer.println(ie.fitnessapp.options.Register.getAge());
+				writer.println(ie.fitnessapp.options.Register.getHeight());
+				writer.println(ie.fitnessapp.options.Register.getWeight());
 				
 				writer.close();
 			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
+				
 				e.printStackTrace();
 			} catch (UnsupportedEncodingException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			
+		}
+		
+			
 			
 			
 		
