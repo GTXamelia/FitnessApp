18bdf937ee32469de2002ded3ddc186e9cc9b6e3
cian2009 <g00337022@gmit.ie>
2018-01-02 16:00:02 +0000

    Server MealRecords commented

--- FitnessServer/src/ie/fitnessapp/options/MealRecords.java
+++ FitnessServer/src/ie/fitnessapp/options/MealRecords.java
@@ -13,6 +13,10 @@ public class MealRecords {
 
 	public static void MealAdd(int clientID, Socket clientSocket, String userDetails, String option1, String option2) throws IOException, ClassNotFoundException {
 		
+		File file = new File("Users/"+userDetails+"/Meal-Records.txt");
+		PrintWriter writer;
+		
+		// Gets the user selection and get the associated string
 		if (option1.equals("1")){
 			option1 = "Breakfast:";
 		}else if (option1.equals("2")){
@@ -25,26 +29,25 @@ public class MealRecords {
 			option1 = "Supper:";
 		}
 		
-		File file = new File("Users/"+userDetails+"/Meal-Records.txt");
-		
-		
+		// Checks if file exists
+		// If file exists information is appended to it line by line
+		// If there is no file found one is created and information appended to it line by line
 		if(file.exists()){
-			PrintWriter writer = new PrintWriter(new FileOutputStream(new File("Users/"+userDetails+"/Meal-Records.txt"), true));
+			writer = new PrintWriter(new FileOutputStream(new File("Users/"+userDetails+"/Meal-Records.txt"), true));
 			
 			writer.println(option2); // Information about meal
 			writer.println(option1); // Meal header
 			
 			writer.close();
 		}else{
-			file.getParentFile().mkdirs();
+			file.getParentFile().mkdirs(); // Create file directory using reference 'file'
 			
-			PrintWriter writer = new PrintWriter(new FileOutputStream(new File("Users/"+userDetails+"/Meal-Records.txt"), true));
+			writer = new PrintWriter(new FileOutputStream(new File("Users/"+userDetails+"/Meal-Records.txt"), true));
 			
 			writer.println(option2); // Information about meal
 			writer.println(option1); // Meal header
 			
 			writer.close();
-			
 		}
 	}
 	
@@ -54,36 +57,44 @@ public class MealRecords {
 		File file = new File("Users/"+userDetails+"/Meal-Records.txt");
 		ArrayList<String> list = new ArrayList<String>();
 		
+		// Variables
 		String line;
 		int stop = 0;
 		
+		// Clears list if any information is present
+		// Stop data being appended twice if run twice
 		list.clear();
 		
+		// If file exists data is read from the file line by line and stored in an array list
+		// If no file is found then nothing will happen
 		if(file.exists()){
 			System.out.println("Client "+clientID+": Address - "+clientSocket.getInetAddress().getHostName()+" - is accessing meal records");
 			
 			// Read from file line by line and add contents to list array
+			// Stops when it reaches end of file
 			while((line = in.readLine()) != null){
 				list.add(line);
 			}
 			
+			// Reset line variable
 			line = "";
-			 for(int i=list.size()-1; i>=0; i--) {
+			
+			// Goes to end of array list get the last 10 elements in the list
+			// If there is less than 10 then it will get all of them
+			for(int i=list.size()-1; i>=0; i--){
 				 
-				 line += list.get(i) + "\n";
+				line += list.get(i) + "\n";// Adds all data from list to a string
 				 
-		         stop++;
+		        stop++;
 		        
 		        if(stop == 20){
 		        	i = 0;
 		        }
-			 }
-			 
-			 System.out.println(line);
-		
+			}
 		}else{
 			System.out.println("Client "+clientID+": Address - "+clientSocket.getInetAddress().getHostName()+" - tried to show list of meals (No meal records)");
+			line = "No meal records found";
 		}
-		in.close();
+		in.close(); // Closes buffered reader
 	}
 }
