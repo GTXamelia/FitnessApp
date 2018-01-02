ba4220e450fc61e199b93a949830d9a93de4ad6e
cian2009 <g00337022@gmit.ie>
2018-01-02 15:14:43 +0000

    Client FileSettings commented

--- FitnessClient/src/ie/fitnessapp/settings/FileSettings.java
+++ FitnessClient/src/ie/fitnessapp/settings/FileSettings.java
@@ -11,44 +11,43 @@ import java.util.Properties;
 import ie.fitnessapp.objects.ServerOptions;
 
 public class FileSettings {
+	
   public static void main(String[] args) {
 
 	Properties prop = new Properties();
 	OutputStream output = null;
 	InputStream input = null;
 	File f = new File("config.properties");
+	ServerOptions ServerOptions = new ServerOptions();
 	
+	// If a config file exists then information will be loaded
+	// Otherwise if a file doesn't exist it will be created and information will be written to
 	if(f.exists()){
-		// load a properties file
+		// Load a properties file
 		try {
 			input = new FileInputStream("config.properties");
 			prop.load(input);
 			
-			ServerOptions ServerOptions = new ServerOptions();
-			
+			// Sets server IP and port to be used in the requester
 			ServerOptions.setServerIp(prop.getProperty("serverip"));
 			ServerOptions.setServerPort(prop.getProperty("serverport"));
-			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}else{
 		try {
-			
 			output = new FileOutputStream("config.properties");
 
-			// set the properties value
+			// Set the properties value
 			prop.setProperty("serverip", "192.168.0.11");
 			prop.setProperty("serverport", "2004");
 			
-			ServerOptions ServerOptions = new ServerOptions();
-			
+			// Set object data
 			ServerOptions.setServerIp("192.168.0.11");
 			ServerOptions.setServerPort("2004");
 
-			// save properties to project root folder
+			// Save properties to project root folder
 			prop.store(output, null);
-
 		} catch (IOException io) {
 			io.printStackTrace();
 		} finally {
