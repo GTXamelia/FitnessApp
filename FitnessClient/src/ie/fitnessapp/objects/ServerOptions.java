5946b09ee9b25dd23b4cd3cfc5f6acfc85c6cd8d
cian2009 <g00337022@gmit.ie>
2018-01-02 14:27:13 +0000

    Commented client ServerOptions

--- FitnessClient/src/ie/fitnessapp/objects/ServerOptions.java
+++ FitnessClient/src/ie/fitnessapp/objects/ServerOptions.java
@@ -1,16 +1,19 @@
 package ie.fitnessapp.objects;
 
+// Used to store server port and IP
 public class ServerOptions {
 	private static String serverIp;
 	private static String serverPort;
 	
-	// Getters And Setters
+	// Getters and setters for server IP
 	public static String getServerIp() {
 		return serverIp;
 	}
 	public void setServerIp(String serverIp) {
 		ServerOptions.serverIp = serverIp;
 	}
+	
+	// Getters and setters for server port
 	public static String getServerPort() {
 		return serverPort;
 	}
