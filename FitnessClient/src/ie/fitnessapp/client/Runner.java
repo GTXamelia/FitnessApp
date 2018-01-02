a6be1d08fb00ff72ffc62ccbb6c471c8b3ec9446
cian2009 <g00337022@gmit.ie>
2018-01-02 14:10:44 +0000

    Comments added

--- FitnessClient/src/ie/fitnessapp/client/Runner.java
+++ FitnessClient/src/ie/fitnessapp/client/Runner.java
@@ -2,12 +2,14 @@ package ie.fitnessapp.client;
 
 import ie.fitnessapp.settings.SetUp;
 
+// Runner is responsible for starting the program
 public class Runner {
 
 	public static void main(String[] args) {
 		
+		// Try SetUp and if any error occurs an error message is output
 		try{
-			SetUp.main(args);
+			SetUp.main(args); // Runs setup which is responsible for making the program run
 		}
 		catch(Exception Exception){
 			System.out.println("Server Error");
--- FitnessServer/src/ie/fitnessapp/server/Runner.java
+++ FitnessServer/src/ie/fitnessapp/server/Runner.java
@@ -1,12 +1,14 @@
 package ie.fitnessapp.server;
 
+// Runner is responsible for running the server
 public class Runner {
 
 	public static void main(String[] args) {
+		
+		// Try EchoServer and if any error occurs it will display a message
 		try {
-			EchoServer.main(args);
+			EchoServer.main(args); // Runs EchoServer which is responsible to communicate with the client(s)
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
